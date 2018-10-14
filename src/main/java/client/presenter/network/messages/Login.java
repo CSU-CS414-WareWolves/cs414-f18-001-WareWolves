3bea014e40a3f71483be9e22cb99e921c829aa26
bcgdwn <robot221@gmail.com>
2018-10-14 15:47:24 -0600

    Added messages sub package, Login as an example class, and the MESSAGE_TYPES enum.

---
+++ src/main/java/client/presenter/network/messages/Login.java
@@ -0,0 +1,36 @@
+package messages;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class Login {
+	//Clas vars
+	private String username;
+	private String passwordAttempt;
+	private InetAddress userIP;
+	private int userPort;
+	protected MESSAGE_TYPE type = MESSAGE_TYPE.LOGIN;
+	
+	
+	public Login(String user, String password, InetAddress ip, int port) {
+		username = user;
+		passwordAttempt = password;
+		userIP = ip;
+		userPort = port;
+	}
+	
+	//Expected: username:password:IP:port
+	public Login(String data) throws UnknownHostException {
+		String[] splt = data.split(":");
+		username = splt[0];
+		passwordAttempt = splt[1];
+		userIP = InetAddress.getByName(splt[2]);
+		userPort = Integer.parseInt(splt[3]);
+	}
+	
+	//Writes out as username:password:IP:port
+	public String getDataString() {
+		return new String(username+":"+passwordAttempt+":"+userIP.toString()+":"+userPort);
+	}
+	
+}
---
+++ src/main/java/client/presenter/network/messages/MESSAGE_TYPE.java
@@ -0,0 +1,30 @@
+package messages;
+
+public enum MESSAGE_TYPE {
+	LOGIN(1),
+	LOGIN_RESPONSE(2),
+	LOGOUT(3),
+	REGISTER(4),
+	UNREGISTER(5),
+	GAME_REQUEST(6),
+	GAME_INFO(7),
+	MOVE(8),
+	ACTIVE_GAMES_REQUEST(9),
+	ACTIVE_GAME_RESPONSE(10),
+	INVITE_REQUEST(11),
+	INVITE_RESPONSE(12);	
+	
+	//Int representation of types
+	private final int typeCode;
+	
+	//Set Message type code
+	MESSAGE_TYPE(int type) {
+		this.typeCode = type;
+	}
+	
+	//Get Message type code
+	public int code() {
+		return this.typeCode;
+	}
+	
+}
