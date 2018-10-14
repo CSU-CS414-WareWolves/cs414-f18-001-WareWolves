e8b213a3caf11718b83d7d1ebb92021d4549562f
Luis <treslar330@gmail.com>
2018-10-14 16:08:48 -0600

    Added a few abstract methods to the interfaces.

--- src/main/java/client/gui/GameView.java
+++ src/main/java/client/gui/GameView.java
@@ -1,5 +1,12 @@
 package client.gui;
 
 public interface GameView {
+  //
+  public void getGameState();
 
+  //
+  //void getValidMove(Point p);
+
+  //
+  //void makeMove(Point from, Point to);
 }
--- src/main/java/client/gui/LoginView.java
+++ src/main/java/client/gui/LoginView.java
@@ -1,5 +1,12 @@
 package client.gui;
 
 public interface LoginView {
+  // Print a nice splash title for our game
+  void showSplash();
 
+  // Log in an existing user
+  void login(String email, String password);
+
+  // Registers a new user
+  void register(String email, String password, String nickname);
 }
