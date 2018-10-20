815cd22b6b5b65c94e856820def4ff1c70dbcb63
Luis <treslar330@gmail.com>
2018-10-20 15:59:40 -0600

    Implemented CLGameView methods to print ASCII characters; added a test method to play around with menu interaction while CL UI is being implemented

--- src/main/java/client/gui/cl/CLMenu.java
+++ src/main/java/client/gui/cl/CLMenu.java
@@ -4,13 +4,8 @@ import client.gui.MenuView;
 import java.util.Scanner;
 
 public class CLMenu implements MenuView {
-  Scanner s;
-  public CLMenu(){
-    s = new Scanner(System.in);
-  }
   public static void clearScreen() {
-    System.out.print("\n\n");
-    System.out.println("-----------------------------------------------------------");
+    System.out.println("\n-----------------------------------------------------------\n");
   }
   // Print out menu for user to select
   public void showMenu(){
@@ -22,10 +17,6 @@ public class CLMenu implements MenuView {
     System.out.println("4.[ View Profile ]");
     System.out.println("5.[  Unregister  ]");
     System.out.println("6.[    Logout    ]");
-  }
-  //solely for testing without anything else?
-  public void handleMenu(){
-
   }
   //
   public void resumeGame(){
