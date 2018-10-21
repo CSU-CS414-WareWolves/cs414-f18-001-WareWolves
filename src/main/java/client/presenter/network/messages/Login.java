1f358ff7d7b44f6ec1783294e1103aa282af64c0
bcgdwn <robot221@gmail.com>
2018-10-21 17:08:22 -0600

    Added LoginResponse, Logout, and tests

--- src/main/java/client/presenter/network/messages/Login.java
+++ src/main/java/client/presenter/network/messages/Login.java
@@ -1,38 +1,38 @@
 package client.presenter.network.messages;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 public class Login extends NetworkMessage{
-	//Clas vars
-	private String username;
+
+	private String email;
 	private String passwordAttempt;
-	private InetAddress userIP;
-	private int userPort;
-	public static final NET_MESSAGE_TYPE type = NET_MESSAGE_TYPE.LOGIN;
 	
-	
-	public Login(String user, String password, InetAddress ip, int port) {
-		username = user;
-		passwordAttempt = password;
-		userIP = ip;
-		userPort = port;
+	/**
+	 * Constructor for Presenter
+	 * @param userEmail email keyed in
+	 * @param passwordHash Hash of keyed in password attempt
+	 */
+	public Login(String userEmail, String passwordHash) {
+		super(NET_MESSAGE_TYPE.LOGIN);
+		email = userEmail;
+		passwordAttempt = passwordHash;
 		length = getDataString().getBytes().length;
 	}
 	
-	//Expected: username:password:IP:port
-	public Login(String data) throws UnknownHostException {
+	/**
+	 * Constructor for Server
+	 * Expected: 1:username:passwordHash
+	 * @param data String representation of the message
+	 */
+	public Login(String data) {
+		super(NET_MESSAGE_TYPE.LOGIN);
 		String[] spilt = data.split(":");
-		username = spilt[0];
-		passwordAttempt = spilt[1];
-		userIP = InetAddress.getByName(spilt[2]);
-		userPort = Integer.parseInt(spilt[3]);
+		email = spilt[1];
+		passwordAttempt = spilt[2];
 		length = getDataString().getBytes().length;
 	}
 	
-	//Writes out as username:password:IP:port
+	@Override
 	public String getDataString() {
-		return new String(type.typeCode+":"+username+":"+passwordAttempt+":"+userIP.toString()+":"+userPort);
+		return new String(type.typeCode+":"+email+":"+passwordAttempt);
 	}
 	
 }
--- src/main/java/client/presenter/network/messages/NetworkMessage.java
+++ src/main/java/client/presenter/network/messages/NetworkMessage.java
@@ -1,8 +1,31 @@
 package client.presenter.network.messages;
 
-//Abstract class for messages, shared functionality
+/**
+ * Abstract class for net messages' shared functionality
+ * @author bcgdwn
+ *
+ */
 public abstract class NetworkMessage {
-	public static final NET_MESSAGE_TYPE type = null;
+	/**
+	 * Message type
+	 */
+	public final NET_MESSAGE_TYPE type;
+	/**
+	 * Length of data String
+	 */
 	public int length; 
+	/**
+	 * 
+	 * @return String form of message
+	 */
 	public abstract String getDataString();
+	
+	/**
+	 * 
+	 * @param Type sets the type field
+	 */
+	public NetworkMessage(NET_MESSAGE_TYPE Type) {
+		type = Type;
+	}
+	
 }
