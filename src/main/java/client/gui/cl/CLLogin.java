200d8c22f224e6853a2032b1659dbc31e97e9c71
Luis <treslar330@gmail.com>
2018-10-25 16:10:00 -0600

    All print statements now construct a single String with StringBuilder and then print that. Overhaul on CLDriver (still in progress); -objectified CLDriver -moved clearScreen() method to CLDriver -altered CLDriver:handleMainMenu -added CLDriver:handleLoginMenu Fixed CLGameView:showGameBoard bug. Implemented CLMenu:viewInvites, requestUsername, showStats, and unregisterUser Corresponding interfaces were also updated with respect to these changes

--- src/main/java/client/gui/cl/CLLogin.java
+++ src/main/java/client/gui/cl/CLLogin.java
@@ -5,37 +5,33 @@ import client.gui.LoginView;
 public class CLLogin implements LoginView {
   //
   public void showSplash() {
-    System.out.println("{  W  E  L  C  O  M  E       T  O  }");
+    StringBuilder res = new StringBuilder();
+    res.append("{  W  E  L  C  O  M  E       T  O  }\n");
+    res.append("####################################\n");
+    res.append("##|  ____|| |##| | / __ \\ |  __ \\ ##\n");
+    res.append("##| |#####| |##| || |##| || |##\\ |##\n");
+    res.append("##| |#####|  __  ||  __  || |###||##\n");
+    res.append("##| |#####| |##| || |##| || |##/ /##\n");
+    res.append("##|______||_|##|_||_|##|_||_____/ ##\n");
+    res.append("####################################\n");
+    res.append("|  ____|| |##| ||  __|| ____||  ___|\n");
+    res.append("| |#####| |##| || |###| |####| |####\n");
+    res.append("| |#####|  __  ||  __||___  ||___  |\n");
+    res.append("| |#####| |##| || |#######| |####| |\n");
+    res.append("|______||_|##|_||____||_____||_____|\n");
+    res.append("####################################\n");
 
-    System.out.println("####################################");
-    System.out.println("##|  ____|| |##| | / __ \\ |  __ \\ ##");
-    System.out.println("##| |#####| |##| || |##| || |##\\ |##");
-    System.out.println("##| |#####|  __  ||  __  || |###||##");
-    System.out.println("##| |#####| |##| || |##| || |##/ /##");
-    System.out.println("##|______||_|##|_||_|##|_||_____/ ##");
-    System.out.println("####################################");
-    System.out.println("|  ____|| |##| ||  __|| ____||  ___|");
-    System.out.println("| |#####| |##| || |###| |####| |####");
-    System.out.println("| |#####|  __  ||  __||___  ||___  |");
-    System.out.println("| |#####| |##| || |#######| |####| |");
-    System.out.println("|______||_|##|_||____||_____||_____|");
-    System.out.println("####################################");
-
-    System.out.println("\n\n{A Chess variant by Christian Freeling (1979)}");
-    System.out.println("{     Implementation by WareWolves (2018)    }");
+    res.append("\n\n{A Chess variant by Christian Freeling (1979)}\n");
+    res.append("{     Implementation by WareWolves (2018)    }\n");
+    System.out.println(res);
   }
-  //
+  /**
+   * Shows the Login screen options
+   */
   public void showLogin() {
-
-    System.out.println("1.[     Login    ]");
-    System.out.println("2.[    Register  ]");
-  }
-  //
-  public void login(String email, String password) {
-
-  }
-  //
-  public void register(String email, String password, String nickname) {
-
+    StringBuilder res = new StringBuilder();
+    res.append("1.[     Login    ]\n");
+    res.append("2.[    Register  ]\n");
+    System.out.println(res);
   }
 }
