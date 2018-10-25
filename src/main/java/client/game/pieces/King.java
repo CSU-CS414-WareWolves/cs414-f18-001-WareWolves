db5412205b7c3427feae2f1ba87bfa394589def0
smilewood <milewood@gmail.com>
2018-10-25 11:35:03 -0600

    Remove extra methods from King find valid moves to reduce complexity.

--- src/main/java/client/game/pieces/King.java
+++ src/main/java/client/game/pieces/King.java
@@ -11,31 +11,18 @@ public class King extends Piece {
 
   /**
    * Finds the valid moves for a King in chad chess. The King cannot leave its castle, and moves as
-   * a combination of a King and Knight. The King can capture any opponent piece that it can land
-   * on.
+   * a combination of a King and Knight in normal chess. The King can capture any opponent piece
+   * that it can land on.
    *
    * @param board A Piece[][] that contains this piece.
    * @return An array of Points containing all valid moves for this King.
    */
   @Override
   public Point[] getValidMoves(Piece[][] board) {
-    ArrayList<Point> result = this.KingMoves(board);
-    result.addAll(KnightMoves(board));
-    return result.toArray(new Point[0]);
-  }
-
-  /**
-   * Finds all the valid knight moves from the current Piece's position give the board containing
-   * the piece.
-   *
-   * @param board A Piece[][] representing the current board and containing this Piece.
-   * @return All legal moves using Knight movement for the King type Piece.
-   */
-  private ArrayList<Point> KnightMoves(Piece[][] board) {
     ArrayList<Point> result = new ArrayList<>();
-    int deltaCol[] = {1, 2, 2, 1, -1, -2, -2, -1};
-    int deltaRow[] = {2, 1, -1, -2, -2, -1, 1, 2};
-    for (int i = 0; i < 8; ++i) {
+    int deltaCol[] = {1, 2, 2, 1, -1, -2, -2, -1, 1, 1, 1, 0, 0, -1, -1, -1};
+    int deltaRow[] = {2, 1, -1, -2, -2, -1, 1, 2, 1, 0, -1, 1, -1, 1, 0, -1};
+    for (int i = 0; i < deltaCol.length; ++i) {
       int col = this.boardLocation.getArrayCol() + deltaCol[i];
       int row = this.boardLocation.getArrayRow() + deltaRow[i];
       Point possible = new Point(col, row);
@@ -43,29 +30,7 @@ public class King extends Piece {
         result.add(possible);
       }
     }
-    return result;
-  }
-
-  /**
-   * Finds all valid King moves from the current Piece's position give the board containing the
-   * piece.
-   *
-   * @param board A Piece[][] representing the current board and containing this Piece.
-   * @return All legal moves using King movement for the King type Piece.
-   */
-  private ArrayList<Point> KingMoves(Piece[][] board) {
-    ArrayList<Point> result = new ArrayList<>();
-    for (int i = -1; i <= 1; ++i) {
-      for (int j = -1; j <= 1; ++j) {
-        int col = this.boardLocation.getArrayCol() + i;
-        int row = this.boardLocation.getArrayRow() + j;
-        Point possible = new Point(row, col);
-        if (this.inOwnCastle(possible) && canMove(board[col][row])) {
-          result.add(possible);
-        }
-      }
-    }
-    return result;
+    return result.toArray(new Point[0]);
   }
 
   /**
