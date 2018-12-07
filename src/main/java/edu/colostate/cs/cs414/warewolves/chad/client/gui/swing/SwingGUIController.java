9d8cb7e5ef624dad94aa23c927e49441fd48ca54
Josiah May <josiahm@rams.colostate.edu>
2018-12-07 12:38:12 -0700

    Comment Abstract class SwingGuiControllers methods

--- src/main/java/edu/colostate/cs/cs414/warewolves/chad/client/gui/swing/SwingGUIController.java
+++ src/main/java/edu/colostate/cs/cs414/warewolves/chad/client/gui/swing/SwingGUIController.java
@@ -5,10 +5,28 @@ import edu.colostate.cs.cs414.warewolves.chad.client.presenter.network.messages.
 import java.awt.event.ActionListener;
 import javax.swing.JPanel;
 
+/**
+ * All Swing panels that either send or receive messages extend from this class
+ * It needs to extend JPanel to implement JPanel methods
+ */
 public abstract class SwingGUIController extends JPanel implements ActionListener {
 
+  /**
+   * Sends a view message to the panels controller
+   * @param message the message to send
+   */
   public abstract void sendMessage(ViewMessage message);
+
+  /**
+   * Receives a view message from its controller to process message
+   * @param message the message to process
+   */
   public abstract void receiveMessage(ViewMessage message);
+
+  /**
+   * Receives a Network Message message from its controller to process message
+   * @param message the message to process
+   */
   public abstract void receiveMessage(NetworkMessage message);
 
 }
