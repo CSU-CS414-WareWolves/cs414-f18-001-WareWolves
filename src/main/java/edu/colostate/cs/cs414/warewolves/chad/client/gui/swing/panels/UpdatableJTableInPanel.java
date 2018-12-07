71286d5b51bb49ec0f5db5b9ada7e824ac79d566
Josiah May <josiahm@rams.colostate.edu>
2018-12-07 12:45:26 -0700

    Comment Abstract class UpdateJTableInPanel methods

--- src/main/java/edu/colostate/cs/cs414/warewolves/chad/client/gui/swing/panels/UpdatableJTableInPanel.java
+++ src/main/java/edu/colostate/cs/cs414/warewolves/chad/client/gui/swing/panels/UpdatableJTableInPanel.java
@@ -4,17 +4,32 @@ import edu.colostate.cs.cs414.warewolves.chad.client.presenter.network.messages.
 import javax.swing.JPanel;
 import javax.swing.JTable;
 
+/**
+ * Any panel that has a JTable in it extends this class to allow the panels controller to update
+ * table
+ */
 public abstract class UpdatableJTableInPanel extends JPanel {
 
+  /**
+   * Uses the information in the Network Message to update its table
+   * @param tableInfo the message with the information
+   */
   public abstract void updateTable(NetworkMessage tableInfo);
 
+  /**
+   * Many of the tables have a hidden ID in the first column, this method gets the value of
+   * the ID or -1 if not row is selected in the table
+   * @param lookupTable the table to look up the ID from
+   * @return the value of the hidden ID or -1 if no row was selected
+   */
   protected int getHiddenID(JTable lookupTable) {
-    int gameIndex = lookupTable.getSelectedRow();
-    if (gameIndex != -1) {
-      return (int) lookupTable.getModel().getValueAt(lookupTable.convertRowIndexToModel(gameIndex), 0);
+    int selectedTableRow = lookupTable.getSelectedRow();
+    // If a row was selected
+    if (selectedTableRow != -1) {
+      return (int) lookupTable.getModel().getValueAt(lookupTable.convertRowIndexToModel(selectedTableRow), 0);
 
     }
-    return gameIndex;
+    return selectedTableRow; // -1
   }
 
 }
