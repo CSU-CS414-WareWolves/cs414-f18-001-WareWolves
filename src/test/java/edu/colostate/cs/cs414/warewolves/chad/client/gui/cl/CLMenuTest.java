741e4134d0b68df506e173dd51d7bc1aa50a8a7a
luisar3 <31224364+luisar3@users.noreply.github.com>
2018-12-09 16:42:42 -0700

    Create CLMenuTest.java

---
+++ src/test/java/edu/colostate/cs/cs414/warewolves/chad/client/gui/cl/CLMenuTest.java
@@ -0,0 +1,85 @@
+package edu.colostate.cs.cs414.warewolves.chad.client.gui.cl;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CLMenuTest {
+
+  private CLMenu menu;
+
+
+  @BeforeEach
+  void setUp() {
+    menu = new CLMenu();
+  }
+
+  @Test
+  void showMenu() {
+    try {
+      menu.showMenu("PLAYER");
+    } catch(Exception e) {
+      e.printStackTrace();
+      fail("");
+    }
+  }
+
+  @Test
+  void viewInvites() {
+    int[] ids = {32,11,75};
+    String[] players = {"Him","Her","Them"};
+    String[] dates = {"today","yesterday","tomorrow"};
+    try {
+      menu.viewInvites(ids,players,dates);
+    } catch(Exception e){
+      e.printStackTrace();
+      fail("");
+    }
+  }
+
+  @Test
+  void requestUsername() {
+    try{
+      menu.requestUsername();
+    } catch(Exception e) {
+      e.printStackTrace();
+      fail("");
+    }
+  }
+
+  @Test
+  void showPlayers() {
+    String[] players = {"theGameMaster", "JosiahMay", "MrGeekTron", "bcgdwn", "luisar3", "smileswood", "AI"};
+    try {
+      menu.showPlayers(players);
+    } catch(Exception e) {
+      e.printStackTrace();
+      fail("");
+    }
+  }
+
+  @Test
+  void showStats() {
+    String player = "RIVAL";
+    String[] white = {"RIVAL"};
+    String[] black = {"YOU"};
+    boolean[] results = {true};
+    try {
+      menu.showStats(player,white,black,results);
+    } catch(Exception e){
+      e.printStackTrace();
+      fail("");
+    }
+  }
+
+  @Test
+  void unregisterUser() {
+    try {
+      menu.unregisterUser();
+    } catch(Exception e) {
+      e.printStackTrace();
+      fail("");
+    }
+  }
+
+}
